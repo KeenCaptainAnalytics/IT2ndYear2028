a062999b18bd9a6f72aa6d0e88ef54ed9474a6c5
pratikjanu <pratikjanu1306@gmail.com>
2025-09-15 22:05:01 +0530

    Update pointersIntro.c
    added comments to explain the concept of pointers to everyone

--- pointersIntro.c
+++ pointersIntro.c
@@ -1,15 +1,15 @@
 // Online C compiler to run C program online
 #include <stdio.h>
 
-void add(int *addr){
-    printf("%p\n" , addr);
-    *addr = *addr + 50 ;
+void add(int *addr){ // addr is a pointer that stores the memory address passed into the function
+    printf("%p\n" , addr);  
+    *addr = *addr + 50 ;  // here *addr is the actual value of the data
     printf("%d\n", *addr);
 }
 int main() {
    int a= 10;
    printf("%d\n", a);
-   add(&a);
+   add(&a);  //passing the address of the data i.e. a into the add function
     printf("%d\n", a);
    return 0;
 }
